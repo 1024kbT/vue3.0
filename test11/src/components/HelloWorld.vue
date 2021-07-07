dc4eb133fbd546844f4e2302e171969a3ea553b9
lwq <843223020@qq.com>
2021-07-07 16:20:26 +0800

    vue3.0

---
+++ test11/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>{{ msg }}</h1>
+    <button></button>
+</template>
+<script>
+export default {
+    emits: ["memberClick"],
+};
+</script>
+<style scoped>
+a {
+    color: #42b983;
+}
+</style>
