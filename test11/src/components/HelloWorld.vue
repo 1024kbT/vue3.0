9719248c6017e608169a9c36512243cb0239a9dd
lwq <843223020@qq.com>
2021-07-07 16:28:20 +0800

    test11

--- test11/src/components/HelloWorld.vue
+++ test11/src/components/HelloWorld.vue
@@ -1,14 +1,14 @@
 <template>
-    <h1>{{ msg }}</h1>
-    <button></button>
+  <h1>{{ msg }}</h1>
+  <button>+++</button>
 </template>
 <script>
 export default {
-    emits: ["memberClick"],
+  emits: ["memberClick"],
 };
 </script>
 <style scoped>
 a {
-    color: #42b983;
+  color: #42b983;
 }
 </style>
